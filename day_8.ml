b24458dcfb75e6705be300f3e673d905b898424a
Max Bickers <mbickers@umich.edu>
2022-12-09 00:35:29 -0500

    Finish day 8

--- day_8.ml
+++ day_8.ml
@@ -2,50 +2,56 @@ open Core
 
 let ( >> ) f g x = g (f x)
 
-let test_input = {|30373
-25512
-65332
-33549
-35390|} |> String.split_lines
-
 let heights =
   In_channel.read_lines "day_8_input.txt"
-  |> List.map ~f:String.to_list
-  |> List.map ~f:(List.map ~f:(String.of_char >> Int.of_string))
+  (* formatting comment *)
+  |> List.map
+       ~f:(String.to_list >> List.map ~f:(String.of_char >> Int.of_string))
 
-let running_maximum =
-  List.folding_map ~init:(-1) ~f:(fun max_seen current ->
-      (Int.max max_seen current, max_seen))
+let visibility =
+  let max_height = 9 in
+  List.folding_mapi ~init:Int.Map.empty ~f:(fun idx height_positions height ->
+      let closest_blocking_idx =
+        Map.fold_range_inclusive height_positions ~min:height ~max:max_height
+          ~init:None ~f:(fun ~key:_ ~data:blocking_idx closest_blocking_idx ->
+            Option.merge (Some blocking_idx) closest_blocking_idx ~f:Int.max)
+      in
+      let visibility =
+        match closest_blocking_idx with
+        | Some blocking_idx -> `Blocked_at_distance (idx - blocking_idx)
+        | None -> `Border_at_distance idx
+      in
+      (Map.set height_positions ~key:height ~data:idx, visibility))
 
-let matrix_to_string matrix ~f =
-  List.map matrix ~f:(List.map ~f >> List.reduce_exn ~f:( ^ ))
-  |> List.intersperse ~sep:"\n" |> List.reduce_exn ~f:( ^ )
-
-let direction_maximums =
-  [
-    List.map ~f:running_maximum heights;
-    List.map ~f:(List.rev >> running_maximum >> List.rev) heights;
-    List.transpose_exn
-      (List.map ~f:running_maximum (List.transpose_exn heights));
-    List.transpose_exn
-      (List.map
-         ~f:(List.rev >> running_maximum >> List.rev)
-         (List.transpose_exn heights));
-  ]
-
-let () =
-  let get ~row_idx ~col_idx matrix =
-    let row = List.nth_exn matrix row_idx in
-    List.nth_exn row col_idx
+let visibilities =
+  let direction_visibilities =
+    [
+      List.map ~f:visibility heights;
+      List.map ~f:(List.rev >> visibility >> List.rev) heights;
+      List.transpose_exn (List.map ~f:visibility (List.transpose_exn heights));
+      List.transpose_exn
+        (List.map
+           ~f:(List.rev >> visibility >> List.rev)
+           (List.transpose_exn heights));
+    ]
   in
-  let visible =
-    List.mapi heights ~f:(fun row_idx row ->
-        List.mapi row ~f:(fun col_idx height ->
-            let direction_maximums =
-              List.map direction_maximums ~f:(get ~row_idx ~col_idx)
-            in
-            let visible = List.exists direction_maximums ~f:(( > ) height) in
-            visible))
-  in
-  let count = List.sum (module Int) ~f:(List.count ~f:Fn.id) visible in
-  printf "a: %d\n" count
+  direction_visibilities |> List.transpose_exn |> List.map ~f:List.transpose_exn
+
+let part_a =
+  List.join visibilities
+  |> List.count
+       ~f:
+         (List.exists ~f:(function
+           | `Border_at_distance _ -> true
+           | `Blocked_at_distance _ -> false))
+  |> printf "a: %d\n"
+
+let part_b =
+  List.join visibilities
+  |> List.map
+       ~f:
+         (List.fold ~init:1 ~f:(fun product visiblity ->
+              match visiblity with
+              | `Border_at_distance distance | `Blocked_at_distance distance ->
+                  product * distance))
+  |> List.reduce_exn ~f:Int.max |> printf "b: %d\n"
