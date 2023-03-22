b18d8e974855b85b7c7f2c0df8ee3bcae856b375
Max Bickers <mbickers@umich.edu>
2023-03-21 22:36:49 -0400

    Version with commoands

--- day_16.ml
+++ day_16.ml
@@ -12,63 +12,163 @@ let parse input =
   in
   String.split_lines input |> List.map ~f:parse_line |> String.Map.of_alist_exn
 
-let memoize (type arg) (module M : Comparable with type t = arg) f =
-  let memo = ref M.Map.empty in
-  let rec g arg =
-    match Map.find !memo arg with
-    | Some value -> value
-    | None ->
-        let value = f ~self:g arg in
-        memo := Map.set !memo ~key:arg ~data:value;
-        value
-  in
-  g
+module Useful_valve_set = struct
+  module T = struct
+    type t = int [@@deriving compare, sexp]
+  end
 
-let part_a valves =
-  let module State = struct
-    module T = struct
-      type t = String.Set.t * String.t * int [@@deriving compare, sexp]
-    end
+  include T
 
-    include T
-    include Comparable.Make (T)
-  end in
-  let solve ~self:solve (open_valves, location, current_time) =
-    let pressure_this_turn =
-      Set.sum
-        (module Int)
-        open_valves
-        ~f:(fun valve ->
-          let flow_rate, _ = Map.find_exn valves valve in
-          flow_rate)
+  let initial_setup ~all_valves =
+    let useful_valve_list =
+      Map.filter all_valves ~f:(fun (flow_rate, _) -> flow_rate > 0)
+      |> Map.to_alist
     in
-    let time_left = 30 - current_time in
-    match time_left = 0 with
-    | true -> pressure_this_turn
-    | false ->
-        let move_scores =
-          let _, possible_locations = Map.find_exn valves location in
-          List.map possible_locations ~f:(fun location ->
-              solve (open_valves, location, current_time + 1))
-        in
-        let stay_score = time_left * pressure_this_turn in
-        let flow_rate_here, _ = Map.find_exn valves location in
-        let open_score =
-          match Set.mem open_valves location || flow_rate_here = 0 with
-          | true -> stay_score
-          | false ->
-              solve (Set.add open_valves location, location, current_time + 1)
-        in
-        let score =
-          List.max_elt ~compare:Int.compare
-            (stay_score :: open_score :: move_scores)
-          |> Option.value_exn
-        in
-        pressure_this_turn + score
-  in
-  let solve = memoize (module State) solve in
-  solve (String.Set.empty, "AA", 1)
+    let useful_valves =
+      List.mapi useful_valve_list ~f:(fun index (valve, _) -> (valve, index))
+      |> String.Map.of_alist_exn
+    in
+    let all_useful_valve_sets =
+      List.init (1 lsl List.length useful_valve_list) ~f:Fun.id
+    in
+    let flow_rates =
+      all_useful_valve_sets
+      |> List.map ~f:(fun open_valves ->
+             let total_flow =
+               List.mapi useful_valve_list
+                 ~f:(fun valve_id (_, (flow_rate, _)) ->
+                   match Int.bit_and open_valves (1 lsl valve_id) <> 0 with
+                   | true -> flow_rate
+                   | false -> 0)
+               |> List.reduce_exn ~f:( + )
+             in
+             (open_valves, total_flow))
+      |> Int.Map.of_alist_exn
+    in
+    (useful_valves, all_useful_valve_sets, flow_rates)
 
+  let empty = 0
+
+  let contains ~useful_valves t valve =
+    match Map.find useful_valves valve with
+    | Some id -> Int.bit_and (1 lsl id) t <> 0
+    | None -> false
+
+  let minus ~useful_valves t valve =
+    assert (contains ~useful_valves t valve);
+    let id = Map.find_exn useful_valves valve in
+    Int.bit_xor (1 lsl id) t
+
+  let overlap t1 t2 = Int.bit_and t1 t2 <> 0
+
+  include Comparable.Make (T)
+end
+
+module State = struct
+  module T = struct
+    type t = String.t * Useful_valve_set.t [@@deriving compare, sexp]
+  end
+
+  include T
+  include Comparable.Make (T)
+end
+
+let best_scores ~all_valves ~useful_valves ~all_useful_valve_sets ~flow_rates =
+  let rec best_scores ~time =
+    match time with
+    | 0 ->
+        let all_states =
+          List.cartesian_product (Map.keys all_valves) all_useful_valve_sets
+        in
+        let all_state_nones =
+          State.Map.of_alist_exn
+            (List.map all_states ~f:(fun state -> (state, None)))
+        in
+        Map.set all_state_nones
+          ~key:("AA", Useful_valve_set.empty)
+          ~data:(Some 0)
+    | time ->
+        let previous_bests = best_scores ~time:(time - 1) in
+        Map.mapi previous_bests
+          ~f:(fun ~key:(position, open_valves) ~data:score ->
+            let stay_in_place =
+              Option.map score ~f:(fun score ->
+                  score + Map.find_exn flow_rates open_valves)
+            in
+            let open_in_place =
+              match
+                Useful_valve_set.contains ~useful_valves open_valves position
+              with
+              | false -> None
+              | true ->
+                  let rest =
+                    Useful_valve_set.minus ~useful_valves open_valves position
+                  in
+                  Option.map
+                    (Map.find_exn previous_bests (position, rest))
+                    ~f:(fun score -> score + Map.find_exn flow_rates rest)
+            in
+            let move_from_neighbor =
+              let _, neighbors = Map.find_exn all_valves position in
+              List.map neighbors ~f:(fun neighbor ->
+                  Map.find_exn previous_bests (neighbor, open_valves)
+                  |> Option.map ~f:(fun score ->
+                         score + Map.find_exn flow_rates open_valves))
+            in
+            let possible_pred_states =
+              stay_in_place :: open_in_place :: move_from_neighbor
+            in
+            List.max_elt
+              ~compare:(Option.compare Int.compare)
+              possible_pred_states
+            |> Option.value_exn)
+  in
+  best_scores
+
+let part_a all_valves =
+  let useful_valves, all_useful_valve_sets, flow_rates =
+    Useful_valve_set.initial_setup ~all_valves
+  in
+  let end_scores =
+    best_scores ~all_valves ~useful_valves ~all_useful_valve_sets ~flow_rates
+      ~time:30
+  in
+  Map.to_alist end_scores
+  |> List.filter_map ~f:(fun (_, score) -> score)
+  |> List.max_elt ~compare:Int.compare
+  |> Option.value_exn
+
+let part_b all_valves =
+  let useful_valves, all_useful_valve_sets, flow_rates =
+    Useful_valve_set.initial_setup ~all_valves
+  in
+  let end_scores =
+    best_scores ~all_valves ~useful_valves ~all_useful_valve_sets ~flow_rates
+      ~time:26
+  in
+  let end_scores =
+    end_scores |> Map.to_alist
+    |> List.filter_map ~f:(fun ((_, open_valves), score) ->
+           Option.map score ~f:(fun score -> (open_valves, score)))
+    |> Useful_valve_set.Map.of_alist_reduce ~f:Int.max
+    |> Map.to_alist
+  in
+  List.cartesian_product end_scores end_scores
+  |> List.filter_map ~f:(fun ((open_valves1, score1), (open_valves2, score2)) ->
+         Option.some_if
+           (not (Useful_valve_set.overlap open_valves1 open_valves2))
+           (score1 + score2))
+  |> List.max_elt ~compare:Int.compare
+  |> Option.value_exn
+
+let () =
+  Command.basic ~summary:"advent of code"
+    (Command.Param.return (fun () ->
+         "day_16_input.txt" |> In_channel.read_all |> parse |> part_b
+         |> printf "%d\n"))
+  |> Command.run
+
+(*
 let%expect_test _ =
   "day_16_input_test.txt" |> In_channel.read_all |> parse |> part_a
   |> printf "%d\n";
@@ -78,168 +178,7 @@ let%expect_test _ =
   "day_16_input.txt" |> In_channel.read_all |> parse |> part_a |> printf "%d\n";
   [%expect "1716"]
 
-(* let part_b valves =
-   let useful_valves =
-     Map.filter valves ~f:(fun (flow_rate, _) -> Int.is_positive flow_rate)
-     |> Map.keys
-   in
-   let useful_valve_subsets =
-     List.init
-       (1 lsl List.length useful_valves)
-       ~f:(fun set_idx ->
-         List.filteri useful_valves ~f:(fun idx _ ->
-             Int.is_positive (Int.bit_and set_idx (1 lsl idx)))
-         |> String.Set.of_list)
-   in
-   let flow_rate open_valves =
-     Set.sum
-       (module Int)
-       open_valves
-       ~f:(fun valve ->
-         let flow_rate, _ = Map.find_exn valves valve in
-         flow_rate)
-   in
-   let module State = struct
-     module T = struct
-       type t = String.Set.t * (String.t * String.t) [@@deriving compare, sexp]
-     end
-
-     include T
-     include Comparable.Make (T)
-   end in
-   let rec scores ~t =
-     match t with
-     | 0 ->
-         let v =
-           List.cartesian_product useful_valve_subsets
-             (List.cartesian_product (Map.keys valves) (Map.keys valves))
-           |> List.map ~f:(fun state -> (state, 0))
-         in
-         State.Map.of_alist_exn v
-     | t ->
-         let prev = scores ~t:(t - 1) in
-         printf "Layer %d\n" t;
-         Map.mapi prev
-           ~f:(fun ~key:(open_valves, (location1, location2)) ~data:_ ->
-             let actions location =
-               let move_actions =
-                 List.map
-                   (let _, tunnels = Map.find_exn valves location in
-                    tunnels)
-                   ~f:(fun location -> `Move location)
-               in
-               match
-                 (not (Set.mem open_valves location))
-                 && List.mem useful_valves location ~equal:String.equal
-               with
-               | true -> `Open location :: move_actions
-               | false -> move_actions
-             in
-             let prev_scores =
-               List.cartesian_product (actions location1) (actions location2)
-               |> List.map ~f:(fun (action1, action2) ->
-                      let open_valves =
-                        match action1 with
-                        | `Open location -> Set.add open_valves location
-                        | _ -> open_valves
-                      in
-                      let open_valves =
-                        match action2 with
-                        | `Open location -> Set.add open_valves location
-                        | _ -> open_valves
-                      in
-                      let locations =
-                        ( (match action1 with
-                          | `Open location | `Move location -> location),
-                          match action2 with
-                          | `Open location | `Move location -> location )
-                      in
-                      Map.find_exn prev (open_valves, locations))
-             in
-             flow_rate open_valves
-             + (List.max_elt prev_scores ~compare:Int.compare |> Option.value_exn))
-   in
-   let begin_scores = scores ~t:26 in
-   Map.find_exn begin_scores (String.Set.empty, ("AA", "AA")) *)
-
-let part_b valves =
-  let useful_valves =
-    Map.filter valves ~f:(fun (flow_rate, _) -> Int.is_positive flow_rate)
-    |> Map.keys
-  in
-  let flow_rate open_valves =
-    Set.sum
-      (module Int)
-      open_valves
-      ~f:(fun valve ->
-        let flow_rate, _ = Map.find_exn valves valve in
-        flow_rate)
-  in
-  let module State = struct
-    module T = struct
-      type t = String.Set.t * String.t * String.t * int
-      [@@deriving compare, sexp]
-    end
-
-    include T
-    include Comparable.Make (T)
-  end in
-  let actions ~open_valves ~location =
-    let move_actions =
-      List.map
-        (let _, tunnels = Map.find_exn valves location in
-         tunnels)
-        ~f:(fun location -> `Move location)
-    in
-    match
-      (not (Set.mem open_valves location))
-      && List.mem useful_valves location ~equal:String.equal
-    with
-    | true -> `Open location :: move_actions
-    | false -> move_actions
-  in
-  let state ~open_valves ~actions:(action1, action2) ~t =
-    let open_valves =
-      match action1 with
-      | `Open location -> Set.add open_valves location
-      | _ -> open_valves
-    in
-    let open_valves =
-      match action2 with
-      | `Open location -> Set.add open_valves location
-      | _ -> open_valves
-    in
-    let location1 =
-      match action1 with `Open location | `Move location -> location
-    in
-    let location2 =
-      match action2 with `Open location | `Move location -> location
-    in
-    (open_valves, location1, location2, t)
-  in
-  let solve ~self:solve (open_valves, location1, location2, current_time) =
-    let time_left = 26 - current_time in
-    match Set.length open_valves = List.length useful_valves with
-    | true -> flow_rate open_valves * (time_left + 1)
-    | false -> (
-        match time_left = 0 with
-        | true -> flow_rate open_valves
-        | false ->
-            let next_states =
-              List.cartesian_product
-                (actions ~open_valves ~location:location1)
-                (actions ~open_valves ~location:location2)
-              |> List.map ~f:(fun actions ->
-                     state ~open_valves ~actions ~t:(current_time + 1))
-            in
-            let scores = List.map next_states ~f:solve in
-            let best_score =
-              List.max_elt scores ~compare:Int.compare |> Option.value_exn
-            in
-            flow_rate open_valves + best_score)
-  in
-  let solve = memoize (module State) solve in
-  solve (String.Set.empty, "AA", "AA", 1)
+let part_b _ = 4
 
 let%expect_test _ =
   "day_16_input_test.txt" |> In_channel.read_all |> parse |> part_b
@@ -249,3 +188,5 @@ let%expect_test _ =
 let%expect_test _ =
   "day_16_input.txt" |> In_channel.read_all |> parse |> part_b |> printf "%d\n";
   [%expect "1707"]
+
+*)
